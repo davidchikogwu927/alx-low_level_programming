8b54b82efaea5f94549e00b853fe2da67b64bc50
davidchikogwu927 <davidchikogwu927@gmail.com>
2023-08-10 18:14:00 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
-#include <stdio.h>
 #include <unistd.h>
+
 /**
- * main - A c program that  prints error
- * return 1 (success)
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-return (1);
+	write(2, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 59);
+	return (1);
 }
